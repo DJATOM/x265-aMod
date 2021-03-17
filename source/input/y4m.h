df37435991f9cec3f3af9a21a8875f1ab89f7b8c
DJATOM <djatom@beatrice-raws.org>
2021-03-17 22:09:09 +0200

    Input: add stopReader() method

--- source/input/y4m.h
+++ source/input/y4m.h
@@ -78,6 +78,7 @@ public:
     bool isEof() const            { return ifs && feof(ifs); }
     bool isFail()                 { return !(ifs && !ferror(ifs) && threadActive); }
     void startReader();
+    void stopReader() {}
     bool readPicture(x265_picture&);
 
     const char *getName() const   { return "y4m"; }
