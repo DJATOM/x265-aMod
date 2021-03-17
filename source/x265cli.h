46ff50d095727e06df382e7aff678639e4a199dd
DJATOM <djatom@beatrice-raws.org>
2021-03-17 22:31:24 +0200

    CLI: add --progress-readframes
    Prints how much frames retrieved from reader

--- source/x265cli.h
+++ source/x265cli.h
@@ -77,6 +77,7 @@ static const struct option long_options[] =
     { "cu-stats",             no_argument, NULL, 0 },
     { "y4m",                  no_argument, NULL, 0 },
     { "no-progress",          no_argument, NULL, 0 },
+    { "progress-readframes",  no_argument, NULL, 0 },
     { "output",         required_argument, NULL, 'o' },
     { "output-depth",   required_argument, NULL, 'D' },
     { "input",          required_argument, NULL, 0 },
@@ -413,6 +414,7 @@ static const struct option long_options[] =
         int64_t startTime;
         int64_t prevUpdateTime;
         const char* readerOpts;
+        bool bReadFrames;
 
         int argCnt;
         char** argString;
@@ -461,6 +463,7 @@ static const struct option long_options[] =
             numRefs = 0;
             argCnt = 0;
             readerOpts = NULL;
+            bReadFrames = false;
         }
 
         void destroy();
