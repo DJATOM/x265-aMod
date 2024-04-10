3c11ecf90b790fe8a351ebe87627dcb3eeec83da
Hari Limaye <hari.limaye@arm.com>
2024-04-10 21:04:05 +0100

    AArch64: Remove explicit use of Neon functions from C
    Remove references to Neon primitives in source/common/pixel.cpp. The
    correct AArch64 optimised primitives are enabled in architecture
    specific files, so these do not belong here.
    
    This also serves to enable compilation when X265_NS is changed by
    removing hard-coded function prefixes.

--- source/common/pixel.cpp
+++ source/common/pixel.cpp
@@ -266,10 +266,6 @@ int satd4(const pixel* pix1, intptr_t stride_pix1, const pixel* pix2, intptr_t s
 {
     int satd = 0;
 
-#if ENABLE_ASSEMBLY && X265_ARCH_ARM64 && !HIGH_BIT_DEPTH
-    pixelcmp_t satd_4x4 = x265_pixel_satd_4x4_neon;
-#endif
-
     for (int row = 0; row < h; row += 4)
         for (int col = 0; col < w; col += 4)
             satd += satd_4x4(pix1 + row * stride_pix1 + col, stride_pix1,
@@ -283,10 +279,6 @@ template<int w, int h>
 int satd8(const pixel* pix1, intptr_t stride_pix1, const pixel* pix2, intptr_t stride_pix2)
 {
     int satd = 0;
-
-#if ENABLE_ASSEMBLY && X265_ARCH_ARM64 && !HIGH_BIT_DEPTH
-    pixelcmp_t satd_8x4 = x265_pixel_satd_8x4_neon;
-#endif
 
     for (int row = 0; row < h; row += 4)
         for (int col = 0; col < w; col += 8)
