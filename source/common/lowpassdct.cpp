7c190672efa662026ffc92bbbbd6dcde20984d06
Pavan Tarun <pavan.tarun@multicorewareinc.com>
2024-08-13 15:27:44 +0530

    Fix lowpassdct negative shift operator

--- source/common/lowpassdct.cpp
+++ source/common/lowpassdct.cpp
@@ -58,7 +58,7 @@ static void lowPassDct8_c(const int16_t* src, int16_t* dst, intptr_t srcStride)
     }
 
     // replace first coef with total block average
-    dst[0] = totalSum >> (-1 + (X265_DEPTH - 8));
+    dst[0] = (X265_DEPTH == 8) ? (totalSum << 1) : (totalSum >> ((X265_DEPTH - 9)));
 }
 
 static void lowPassDct16_c(const int16_t* src, int16_t* dst, intptr_t srcStride)
